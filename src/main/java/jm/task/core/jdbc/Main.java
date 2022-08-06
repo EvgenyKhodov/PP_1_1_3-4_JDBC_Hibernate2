f4685a75a3a8b664b1bb8933a053a94348ee6815
Евгений <Khodov1992@mail.ru>
2022-08-06 22:32:19 +0500

    commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,23 @@
 package jm.task.core.jdbc;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Евгений", "Севостьянов", (byte) 28);
+        userService.saveUser("Иван", "Петров", (byte) 31);
+        userService.saveUser("Сергей", "Смирнов", (byte) 24);
+        userService.saveUser("Владислав", "Иванов", (byte) 22);
+        System.out.println(userService.getAllUsers());
+        userService.removeUserById(1);
+        System.out.println(userService.getAllUsers());
+        userService.getAllUsers();
+        System.out.println(userService.getAllUsers());
+        userService.cleanUsersTable();
+        System.out.println(userService.getAllUsers());
+        userService.dropUsersTable();
     }
 }
+
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,20 @@
+//DAO (data access object) — один из наиболее распространенных паттернов проектирования, "Доступ к данным". Его смысл прост — создать в приложении слой, который отвечает только за доступ к данным
+
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;//Интерфейс org.hibernate.Session является мостом между приложением и Hibernate
 
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Transactional //Указывает, что свойство или поле не являются постоянными.
 public class UserDaoHibernateImpl implements UserDao {
+
+    private final Session session = Util.getSessionFactory().openSession();//соединение с бд, рабоатет с объектами
+    final String sqlQuery = "CREATE TABLE IF NOT EXISTS users (`id` INT NOT NULL AUTO_INCREMENT,`name` VARCHAR(45) NULL,`lastName` VARCHAR(45) NULL,`age` VARCHAR(45) NULL, PRIMARY KEY (`id`),UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);";
     public UserDaoHibernateImpl() {
 
     }
@@ -13,30 +23,81 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void createUsersTable() {
 
+        try {
+            session.beginTransaction();//проверит, есть ли уже существующая транзакция, если да, то не создаст новую транзакцию
+            session.createSQLQuery(sqlQuery).executeUpdate();//транзакция.фиксация();
+            session.getTransaction().commit();//возвращаемый результат;
+        } catch (HibernateException e) {
+            e.getStackTrace();
+            session.close();
+        }
     }
 
     @Override
     public void dropUsersTable() {
+        String sql = "DROP TABLE IF EXISTS users;";//
 
+        try {
+            session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.getStackTrace();
+            session.close();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try {
+            session.beginTransaction();
+            session.save(new User(name, lastName, age));//создать
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.getStackTrace();
+            session.close();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        String hql = "DELETE User WHERE id = :id";
+        try {
+            session.beginTransaction();
+            session.delete(session.get(User.class,id));//удалить
+            session.createQuery(hql);
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.getStackTrace();
+            session.close();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List users = null;
+
+        try {
+            users = session.createQuery("FROM User").list();
+        } catch (HibernateException e) {
+            e.getStackTrace();
+            session.close();
+        }
+
+        return users;
     }
 
     @Override
     public void cleanUsersTable() {
+        String hql = "delete User";
 
+        try {
+            session.beginTransaction();
+            session.createQuery(hql).executeUpdate();
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.getStackTrace();
+            session.close();
+        }
     }
 }
